db2ce906e663d8ef2a129f2564b90ea88c626147
Nicolas Ippolito <Nippolito@ws16.labo2>
2017-05-05 16:05:00 -0300

    Ahora si anda el ejercicio 2, tenia algunos errores. Lo testie con 2 casos y da perfecto. Si alguien quiere dejarlo bien pulido, perfecto

--- Ej2.cpp
+++ Ej2.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <stdlib.h> 
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
 
 using namespace std;
 
-
 // Represento a un eje
-struct Edge
+struct Edge 				
 {
     int src, dest, weight;
 };
  
-
 // Represento al grafo
-struct Graph
+struct Graph 			    // n-> Number of vertices, m-> Number of edges
 {
-    // n-> Number of vertices, m-> Number of edges
     int n, m;
- 
-    // graph is represented as an array of edges.
-    struct Edge* edge;
+    struct Edge* edge;		   // graph is represented as an array of edges.
 };
 
 
@@ -35,18 +32,16 @@ struct Graph* createGraph(int n, int m)
 }
 
 
-
-
-// suma todos los ejes y le suma 1 para poder utilizar esto como el "infinito"
+// suma todos los ejes y le suma 1000 para poder utilizar esto como el "infinito"
 int sumaEjes(struct Graph* grafo){
 	int res = 0;
 	for(int i = 0; i < grafo->m; i++){
 		res = res + grafo->edge[i].weight;
 	}
-	return res + 1;
+	return res + 1000;
 }
 
-
+// calcula el peaje máximo
 int maxPeaje(struct Graph* grafo){
 	int res = grafo->edge[0].weight;
 	for(int i = 1; i < grafo->m; i++){
@@ -57,11 +52,17 @@ int maxPeaje(struct Graph* grafo){
 	return res;
 }
 
+void mostrarPesos(struct Graph* grafo){			// al pedo
+	for(int i = 0; i < grafo->m; i++){
+		cout << grafo->edge[i].weight << endl;
+	}
+}
 
-bool fordCicloNegativo(struct Graph* grafo, int source){
+
+bool fordCicloNegativo(struct Graph* grafo, int source, int c){
 	int n = grafo->n;
 	int m = grafo->m;
-	int dist[n];
+	vector<int> dist(n);
 
 	int sumEjes = sumaEjes(grafo);
 
@@ -76,7 +77,7 @@ bool fordCicloNegativo(struct Graph* grafo, int source){
 		for(int j = 0; j < m; j++){
 			int u = grafo->edge[j].src;
 			int v = grafo->edge[j].dest;
-			int weight = grafo->edge[j].weight;
+			int weight = grafo->edge[j].weight - c;
 			if(dist[u] != sumEjes && dist[u] + weight < dist[v]){
 				dist[v] = dist[u] + weight;
 			}
@@ -88,7 +89,7 @@ bool fordCicloNegativo(struct Graph* grafo, int source){
 	for(int j = 0; j < m; j++){
 		int u = grafo->edge[j].src;
 		int v = grafo->edge[j].dest;
-		int weight = grafo->edge[j].weight;
+		int weight = grafo->edge[j].weight - c;
 		if(dist[u] != sumEjes && dist[u] + weight < dist[v]){
 			b = true;
 		}
@@ -100,35 +101,102 @@ bool fordCicloNegativo(struct Graph* grafo, int source){
 
 int Ej2(struct Graph* grafo){
 	int c = maxPeaje(grafo);
-	int arr[c];
-
+	vector<int> arr(c);
 	// inicializo el arreglo, qué fácil que sería en python esto!
 	for(int i = 0; i < c; i++){
 		arr[i] = i + 1;
 	}
+
 	int L = 0;
 	int R = c - 1;
-	int m = (L + R) / 2;			// falta que tome el piso o techo
+	int m = floor((L + R) / 2);
 	bool b;
 
 	// búsqueda binaria + Ford
-	while(L < R){
-		b = fordCicloNegativo(grafo, m);
+	while(L+1 < R){
+		b = fordCicloNegativo(grafo, 0, arr[m]);
 		if(b){
 			R = m;
 		}else{
 			L = m;
 		}
-		m = (L + R) / 2;
+		m = floor((L + R) / 2);
+ 	}
+	bool h = fordCicloNegativo(grafo, 0, arr[R]);
+	if(h){
+		return arr[L];
+	}else{
+		return arr[R];
 	}
-	return L;
 }
 
 
+void Test1(){			// debe dar 2
+	int n = 4;
+	int m = 4;
+	struct Graph* grafo = createGraph(n, m);
 
+	grafo->edge[0].src = 1;
+	grafo->edge[0].dest = 0;
+	grafo->edge[0].weight = 1;
 
+	grafo->edge[1].src = 0;
+	grafo->edge[1].dest = 3;
+	grafo->edge[1].weight = 5;
+
+	grafo->edge[2].src = 3;
+	grafo->edge[2].dest = 2;
+	grafo->edge[2].weight = 1;
+
+	grafo->edge[3].src = 2;
+	grafo->edge[3].dest = 1;
+	grafo->edge[3].weight = 1;
+
+	int h = Ej2(grafo);
+	cout << "La respuesta es: " << h << endl;
+}
+
+void Test2(){			// debe dar 16
+	int n = 6;
+	int m = 7;
+	struct Graph* grafo = createGraph(n, m);
+
+	grafo->edge[0].src = 0;
+	grafo->edge[0].dest = 1;
+	grafo->edge[0].weight = 30;
+
+	grafo->edge[1].src = 1;
+	grafo->edge[1].dest = 2;
+	grafo->edge[1].weight = 10;
+
+	grafo->edge[2].src = 2;
+	grafo->edge[2].dest = 0;
+	grafo->edge[2].weight = 10;
+
+	grafo->edge[3].src = 2;
+	grafo->edge[3].dest = 3;
+	grafo->edge[3].weight = 100;
+
+	grafo->edge[4].src = 3;
+	grafo->edge[4].dest = 5;
+	grafo->edge[4].weight = 100;
+
+	grafo->edge[5].src = 4;
+	grafo->edge[5].dest = 3;
+	grafo->edge[5].weight = 100;
+
+	grafo->edge[6].src = 4;
+	grafo->edge[6].dest = 5;
+	grafo->edge[6].weight = 100;
+
+	int h = Ej2(grafo);
+	cout << "La respuesta es: " << h << endl;
+}
 
 
 int main(){
+	Test1();
+	Test2();
+
 	return 0;
 }
