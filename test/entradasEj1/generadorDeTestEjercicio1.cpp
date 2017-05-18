4004cac07548faca0ea08a966e5b81f316aead0d
Gregorio <gregorio.freidin@gmail.com>
2017-05-17 21:11:01 -0300

    blabla

--- test/entradasEj1/generadorDeTestEjercicio1.cpp
+++ test/entradasEj1/generadorDeTestEjercicio1.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 using namespace std;
+int matriz[40][40];
 
 int main ()
 {
@@ -16,27 +17,41 @@ int main ()
 
   /* generate secret number between 1 and 10: */
 
-  for(int z = 0; z < 50; z++){
+  for(int z = 0; z < 10; z++){
     n = rand() %  30 + 10;
     int aux = n* (n-1) / 2  -1;
     int m =  rand() % aux + 1;
 
-    int o = rand() % n;
-    int d = rand() % n;
+    int o = rand() % n + 1;
+    int d = rand() % n + 1;
     if(d == o) o = d-1; // por si tengo la mala leche de que me salgan la misma ciudad.
     int k = rand() % 20 + 8 ;
     int cantPremiums = rand() % m/2;
 
+
+    for(int i = 0 ; i < n; i ++){
+      for(int j = 0 ; j < n; j ++) matriz[i][j]= -1;
+    }
+
     f << n <<" " << m <<" " << o << " " << d << " " << k << endl;
     for(int i = 0; i < m  ; i ++){
-      int c1 = rand() % n;
-      int c2 = rand() % n;
+      int c1 = rand() % n + 1;
+      int c2 = rand() % n + 1;
       if(c1 == c2) c1 = c2-1;
       int p ;
       if(i < cantPremiums) p = 1;
       else p = 0;
       int dist = rand() % 20 + 1;
-      f << c1 << " " << c2 << " " << p << " " << dist << endl;
+      
+
+      if(matriz [c1][c2] == -1 && c1 != c2){
+        matriz[c1][c2] = 1;
+        matriz[c2][c1] = 1;
+        f << c1 << " " << c2 << " " << p << " " << dist << endl;
+      }
+      else{
+        i=i-1; //try again bro
+      }
     }
     f << "\n\n\n\n\n\n";
   }
