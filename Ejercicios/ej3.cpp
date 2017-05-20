05e49e60110e9aff5115bdd59847a7bbb9b28ff6
Emiliano <noggaxiii@gmail.com>
2017-05-20 13:10:27 -0300

    Actualizacion ej3. Ahora compila y funciona 'pipi cucu'

--- Ejercicios/ej3.cpp
+++ Ejercicios/ej3.cpp
@@ -17,10 +17,9 @@ A la par, ir sumando las aristas que voy agregando a cantAristas.
 
 Devolver [cantAristas, mst_constructed2Remove+mst_build];
 
-
-
-
 */
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -63,12 +62,12 @@ struct Graph
 	// Function to find MST using Kruskal's
 	// MST algorithm.
 	//Devuelve el costo y tambien cuantas aristas tiene
-	int[2] kruskalMST();
+	iPair kruskalMST();
 };
 
 
 // Disjoint Sets va a representar las componentes conexas creadas.
-//Aca hay algo de magia, pero leer con detenimiento que las complejidades cierran.
+//Aca hay algo de magia, pero leer con detenimiento que las complejidades cierran. (Kruskal lo garantiza)
 //+info: http://www.csl.mtu.edu/cs4321/www/Lectures/Lecture%2019%20-%20Kruskal%20Algorithm%20and%20Dis-joint%20Sets.htm
 
 struct DisjointSets
@@ -123,9 +122,8 @@ struct DisjointSets
 	}
 };
 
-/* Functions returns weight of the MST*/
 
-int[2] Graph::kruskalMST()
+iPair Graph::kruskalMST()
 {
 	int mst_build = 0; // Contador del costo por CONSTRUIR
 	int mst_constructed2Remove = 0; //Contador del costo por REMOVER
@@ -201,9 +199,9 @@ int[2] Graph::kruskalMST()
 		}
 	}
 
-	int[2] res;
-	res[0] = cantAristas;
-	res[1] = mst_build + mst_constructed2Remove;
+	iPair res;
+	res.first = cantAristas;
+	res.second = mst_build + mst_constructed2Remove;
 	return res;
 }
 
@@ -212,29 +210,28 @@ int main()
 {
 	/* Let us create above shown weighted
 	and unidrected graph */
-	int V = 9, E = 14;
+	int V = 5, E = 20;
 	Graph g(V, E);
 
 	// making above shown graph. el eje posee 4 parametros (construido/noconstruido; nodo origen; nodo destino; peso )
-	g.addEdge(1, 0, 1, 4);
-	g.addEdge(1, 0, 7, 8);
-	g.addEdge(1, 1, 2, 8);
-	g.addEdge(1, 1, 7, 11);
-	g.addEdge(1, 2, 3, 7);
-	g.addEdge(1, 2, 8, 2);
-	g.addEdge(1, 2, 5, 4);
-	g.addEdge(0, 3, 4, 9);
-	g.addEdge(0, 3, 5, 14);
-	g.addEdge(0, 4, 5, 10);
-	g.addEdge(0, 5, 6, 2);
-	g.addEdge(0, 6, 7, 1);
-	g.addEdge(0, 6, 8, 6);
-	g.addEdge(0, 7, 8, 7);
+	g.addEdge(0, 0, 1, 4);
+	g.addEdge(1, 0, 2, 8);
+	g.addEdge(1, 0, 3, 8);
+	g.addEdge(1, 0, 4, 11);
 
+	g.addEdge(0, 1, 2, 2);
+	g.addEdge(0, 1, 3, 4);
+	g.addEdge(0, 1, 4, 9);
+	
+	g.addEdge(0, 2, 3, 14);
+	g.addEdge(0, 2, 4, 10);
+	
+	g.addEdge(1, 3, 4, 2);
+	
 	cout << "Los ejes de las ciudades son \n";
-	int[2] res = g.kruskalMST();
-	cout << "\n El costo total es " << res[1];
-	cout << "\n El numero de vertices es " << res[0];
+	iPair res = g.kruskalMST();
+	cout << "\n El costo total es " << res.second;
+	cout << "\n El numero de vertices es " << res.first << endl;
 
 	return 0;
 }
