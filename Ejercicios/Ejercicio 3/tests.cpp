f79d3380be20abf0949f1efb5421ada9761517f3
Peter <megalovania@ubuntu>
2017-05-21 11:41:11 -0300

    test ahora genera muestras

--- Ejercicios/Ejercicio 3/tests.cpp
+++ Ejercicios/Ejercicio 3/tests.cpp
@@ -6,46 +6,61 @@
 
 using namespace std;
 
-void Test1(){
-	int V = 5, E = 20;
-	Graph g(V, E);
+void CrearGrafoCompleto(Graph& g){
 	
-	// making above shown graph. el eje posee 4 parametros (construido/noconstruido; nodo origen; nodo destino; peso )
-	g.addEdge(0, 1, 0, 4);
-	g.addEdge(0, 2, 1, 8);
-	g.addEdge(0, 3, 1, 8);
-	g.addEdge(0, 4, 1, 11);
-
-	g.addEdge(1, 2, 0, 2);
-	g.addEdge(1, 3, 0, 4);
-	g.addEdge(1, 4, 0, 9);
-	
-	g.addEdge(2, 3, 0, 14);
-	g.addEdge(2, 4, 0, 10);
-	
-	g.addEdge(3, 4, 1, 2);
-	
-	cout << "Los ejes de las ciudades son \n";
-	iPair res = g.kruskalMST();
-	vector<iPair> sol = g.dameSolucion();
-	
-	vector<iPair>::iterator itConst;
-	for (itConst = sol.begin(); itConst!=sol.end(); itConst++) 	{
-		int u = itConst->first;
-		int v = itConst->second;
-		cout <<u <<" "<< v <<" ";
+	for (int i = 0; i < g.V; ++i)
+	{
+		for (int j = i+1; j < (g.V -1); ++j)
+		{
+			g.addEdge(i,j, rand() % 2, rand() % 101);
+		}		
 	}
-	
-	cout << endl;
-	cout << "\n El costo total es " << res.second;
-	cout << "\n El numero de vertices es " << res.first << endl;
+}
 
+void TestMediciones(){
+
+	auto start = std::chrono::high_resolution_clock::now();
+    auto finish = std::chrono::high_resolution_clock::now();
+
+    stringstream ss;
+    ss <<  "Muestreo_Ej3" <<".txt";
+
+	ofstream file( ss.str() );
+    if ( !file )
+	{
+        cerr << "Error: failed to create file " << ss.str() << '\n';
+ 	}
+
+    int cantMuestras = 100;
+	for (int i = 2; i < 200; ++i)
+	{
+		typedef std::chrono::duration<double, std::milli> Duracion;
+		double promedioMili = 0;
+		for (int j = 0; j < cantMuestras; ++j)
+		{
+			Graph g(i, i * (i-1) / 2);
+			CrearGrafoCompleto(g);
+
+			start = std::chrono::high_resolution_clock::now();
+   			iPair res = g.kruskalMST();
+			finish = std::chrono::high_resolution_clock::now();
+	   		Duracion tiempoEjec = finish - start;
+		   	promedioMili += tiempoEjec.count();
+
+		}
+		promedioMili = promedioMili/cantMuestras;
+		file << i << " " << promedioMili << "\n";
+		cout << "Voy por cantidad de nodos: " << i << ". El promedio fue: " << promedioMili << endl;
+	}
+		
+	
+	
 }
 
 
 
 int main()
 {
-	Test1();	
+	TestMediciones();	
 	return 0;
 }
